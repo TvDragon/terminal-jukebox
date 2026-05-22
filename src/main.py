8781c1a0b851b8be6bfcbbf6c4d2674c5ce2c6fe
Tony Vo <tonyvo249@gmail.com>
2026-05-22 20:14:25 +1000

    search for song

--- src/main.py
+++ src/main.py
@@ -30,11 +30,24 @@ from textual.widgets import (
 )
 from textual.widgets.selection_list import Selection
 
+import unicodedata
+
 from database_handler import SQL_Connector
 
-# --- Music Table View --------------------------------------------------------
+def normalise_text(text: str) -> str:
+	# NFD = Normalization Form Decomposed
+	# Mn = Mark, Nonspacing
+	normalised_text = unicodedata.normalize("NFD", text)
 
-# --- Main Application --------------------------------------------------------
+	final_text = ""
+	for character in normalised_text:
+		category = unicodedata.category(character)
+		if category != "Mn":
+			final_text += character
+
+	return final_text.lower()
+
+# --- Music Table View --------------------------------------------------------
 
 class MusicTable(DataTable):
 
@@ -55,6 +68,9 @@ class MusicTable(DataTable):
 			song_duration = "{}:{:02d}".format(minutes, seconds)
 			self.add_row(song["id"], song["title"], song["artist"], song["album"], song["genres"], song_duration)
 
+
+# --- Main Application --------------------------------------------------------
+
 class TerminalJukeBox(App):
 	"""Terminal Jukebox - Music Player"""
 	
@@ -71,7 +87,9 @@ class TerminalJukeBox(App):
 		self.home_image_path = "assets/home.png"
 		self.sql_db_connector = SQL_Connector()
 		self.all_songs = self.sql_db_connector.get_all_songs()
-		self.playlist_name = "Vinahouse"
+		self.playlist_songs = []
+		self.playlist_name = None
+		self.current_tab = "tab-music"
 
 	def on_mount(self):
 		table = self.query_one("#music-table", MusicTable)
@@ -82,25 +100,23 @@ class TerminalJukeBox(App):
 
 	def load_playlist(self) -> list:
 		if self.playlist_name != None:
-			return self.sql_db_connector.get_songs_in_playlist(self.playlist_name)
-		return []
+			self.playlist_songs = self.sql_db_connector.get_songs_in_playlist(self.playlist_name)
+		return self.playlist_songs
 
 	def compose(self) -> ComposeResult:
 		yield Header()
 
 		with Container(id="root"):
 			# --- Top Bar (Home and search bar) ---
-			with Container(id="top-box", classes="box"):
+			with Container(id="top-box"):
 				with Horizontal(id="top-bar", classes="main-border"):
 					yield Button("Scan", id="btn-scan")
-					yield Button("Home", id="btn-home")
 					yield Input(
 						placeholder="Search for music...",
 						id="music-search-input"
 					)
-					yield Button("Search", id="btn-search")
 
-			with Container(id="main-box", classes="box main-border"):
+			with Container(id="main-box", classes="main-border"):
 				# --- Middle Section (Playlists and songs) ---
 				with TabbedContent("Music", "Playlists", id="tabs"):
 					# --- All Songs Tab ---
@@ -128,10 +144,28 @@ class TerminalJukeBox(App):
 			self.load_songs()
 			table.set_songs(self.all_songs)
 		elif tab_id == "tab-playlists":
-
 			table = self.query_one("#songs-playlist-table", MusicTable)
 			songs = self.load_playlist()
 			table.set_songs(songs)
+		self.current_tab = tab_id
+
+	@on(Input.Changed, "#music-search-input")
+	def update_search_music(self, event: Input.Changed) -> None:
+		searched_music = normalise_text(event.value)
+
+		results = []
+		table = None
+		if self.current_tab == "tab-music":
+			for song in self.all_songs:
+				if searched_music in normalise_text(song["title"]):
+					results.append(song)
+			table = self.query_one("#music-table", MusicTable)
+		elif self.current_tab == "tab-playlists":
+			for song in self.playlist_songs:
+				if searched_music.lower() in normalise_text(song["title"]):
+					results.append(song)
+			table = self.query_one("#songs-playlist-table", MusicTable)
+		table.set_songs(results)
 
 # --- Entry Point -------------------------------------------------------------
 
